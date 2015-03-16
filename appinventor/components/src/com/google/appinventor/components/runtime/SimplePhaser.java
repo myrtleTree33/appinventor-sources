70537856dd5354eceb4a3c7017b56859879e6248
Joel Tong <me@joeltong.org>
2015-03-16 23:53:37 +0800

    successfully loaded SimpplePhaser app and webpage for first time

--- appinventor/components/src/com/google/appinventor/components/runtime/SimplePhaser.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/SimplePhaser.java
@@ -1,16 +1,20 @@
 package com.google.appinventor.components.runtime;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.webkit.JavascriptInterface;
 import com.google.appinventor.components.annotations.*;
 import com.google.appinventor.components.common.ComponentCategory;
 import com.google.appinventor.components.common.PropertyTypeConstants;
 import com.google.appinventor.components.common.YaVersion;
+import com.google.appinventor.components.runtime.util.FroyoUtil;
+import com.google.appinventor.components.runtime.util.SdkLevel;
 
 /**
  * Created by joel on 3/12/15.
@@ -34,12 +38,15 @@ public class SimplePhaser extends AndroidViewComponent {
    */
   public class WebViewInterface {
     Form webViewForm;
+    Context mContext;
+    String  webViewString;
 
     /**
      * Instantiate the interface and set the context
      */
-    WebViewInterface(Form webViewForm) {
-      this.webViewForm = webViewForm;
+    WebViewInterface(Context c) {
+      mContext = c;
+      webViewString = " ";
     }
   }
 
@@ -54,10 +61,11 @@ public class SimplePhaser extends AndroidViewComponent {
 
     this.form = container.$form();
     webview = new WebView(container.$context());
+    resetWebViewClient();       // Set up the web view client
     webview.getSettings().setJavaScriptEnabled(true);
     webview.setFocusable(true);
 // adds a way to send strings to the javascript
-    wvInterface = new WebViewInterface(form);
+    wvInterface = new WebViewInterface(webview.getContext());
     webview.addJavascriptInterface(wvInterface, "AppInventorMap");
 //We had some issues with rendering of maps on certain devices; using caching seems to solve it
     webview.setDrawingCacheEnabled(false);
@@ -95,19 +103,50 @@ public class SimplePhaser extends AndroidViewComponent {
 
   }
 
+  // Create a class so we can override the default link following behavior.
+  // The handler doesn't do anything on its own.  But returning true means that
+  // this do nothing will override the default WebVew behavior.  Returning
+  // false means to let the WebView handle the Url.  In other words, returning
+  // true will not follow the link, and returning false will follow the link.
+  private class WebViewerClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+      boolean followLinks = true;
+      return !followLinks;
+    }
+  }
+
+  private void resetWebViewClient() {
+    if (SdkLevel.getLevel() >= SdkLevel.LEVEL_FROYO) {
+      boolean ignoreSslErrors = false;
+      boolean followLinks = true;
+      webview.setWebViewClient(FroyoUtil.getWebViewClient(ignoreSslErrors, followLinks, container.$form(), this));
+    } else {
+      webview.setWebViewClient(new WebViewerClient());
+    }
+  }
+
 
 //  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
 //          defaultValue = "")
   @SimpleProperty
-  public void LoadWebpage(String doNothing) {
+  public void GetSomeProperty(String doNothing) {
     //Do nothing with doNothing :)
+//    loadPage();
+  }
+
+  @SimpleFunction(
+          description = "Loads the Google page."
+  )
+  public void LoadWebpage(String doNothing) {
     loadPage();
   }
 
 
   public void loadPage() {
-    String markup = "<HTML>hello world</HTML>";
-    webview.loadDataWithBaseURL(null, markup, "text/html", "utf-8", null);
+    String markup = "<HTML>hello world!! Hihi there!!</HTML>";
+//    webview.loadDataWithBaseURL(null, markup, "text/html", "utf-8", null);
+    webview.loadUrl("http://joeltong.org/phaser/phaser.html");
   }
 
   @Override
@@ -134,4 +173,5 @@ public class SimplePhaser extends AndroidViewComponent {
     }
     super.Height(height);
   }
+
 }
